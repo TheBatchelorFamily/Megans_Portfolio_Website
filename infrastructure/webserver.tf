a36f3838ee8c5051e956a83a0ec503c3b9a8e95a
christsreturn <christhebatchelor@gmail.com>
2022-08-01 16:21:50 -0500

    Migrating to new repo and adding terraform deployment (#1)
    * Stage initial build
    
    * working terraform checkin
    
    * remove secrets for testing
    
    * ssh -> https
    
    * fix push path for terraform deploy
    
    * fix module reference
    
    * fix push path for terraform deploy take 2
    
    * fix required terraform version
    
    * fix push path for terraform deploy take 3
    
    * secrets inherit for terraform deploy
    
    * further secrets testing
    
    * add ts:skip for s3 versioning
    
    * lint tweaks
    
    * further lint tweaks for bootstrap

---
+++ infrastructure/webserver.tf
@@ -0,0 +1,61 @@
+terraform{
+  backend "s3" {
+    bucket = "terraform-itsmeganificent"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.22.0"
+    }
+  }
+  required_version = "~> 1.2.5"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  #ts:skip=AC_AWS_0214 Enabling S3 versioning
+  bucket = "terraform-itsmeganificent"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+module "aws_webserver_network" {
+  source       = "github.com/TheBatchelorFamily/SharedTerraform.git//modules/aws_webserver_network?ref=module_build"
+  dnsName      = var.dnsName
+  r53Enabled   = true
+  region       = var.region
+  secgroupname = var.secgroupname
+  sshIP        = var.sshIP
+  tags         = var.tags
+  vpc          = var.vpc
+}
+
+module "aws_auto_scale" {
+  keyname       = var.keyname
+  publicIP      = true
+  securityGroup = [module.aws_webserver_network.aws_security_group_id]
+  source        = "github.com/TheBatchelorFamily/SharedTerraform.git//modules/aws_auto_scale?ref=module_build"
+  sshPub        = file(var.sshPub)
+  subnet        = var.subnet
+  tags          = var.tags
+  userData      = base64encode(templatefile(
+    "./user_data.tftpl",
+    {
+      eipID:module.aws_webserver_network.aws_eip_alloID,
+      region:var.region
+    }
+  ))
+}
