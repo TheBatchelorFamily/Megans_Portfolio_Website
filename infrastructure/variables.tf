7a369946d0e34f8f281c632ad25b482a8c64e729
Chris Batchelor <christhebatchelor@gmail.com>
2025-12-15 11:46:30 -0600

    Move alerting to module

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -27,9 +27,3 @@ variable "publicIP" {
   type        = bool
   default     = true
 }
-
-variable "alert_email" {
-  description = "Email address to receive CloudWatch alerts"
-  type        = string
-  default     = "christsreturn01@gmail.com"
-}
