ca671d3559fa6f1fa48cded1f621f343aac3126b
Chris Batchelor <christhebatchelor@gmail.com>
2026-04-20 16:09:28 -0500

    Revert "Point internal portfolio to beta"
    This reverts commit 63331722239094bf024eb250c2792e3cf7013af7.

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -7,10 +7,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.41"
+      version = "~> 5.98"
     }
   }
-  required_version = "~> 1.14"
+  required_version = "~> 1.12"
 }
 
 provider "aws" {
